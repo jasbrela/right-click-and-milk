9fbe1cdd93f87a780aad7d179bca79bb69c74e94
Gabriela Matias <jasbrela@gmail.com>
2025-05-18 00:13:50 -0300

    remove template config

--- src/main/java/com/bobossaura/rightclickandmilk/RightClickAndMilk.java
+++ src/main/java/com/bobossaura/rightclickandmilk/RightClickAndMilk.java
@@ -58,9 +58,6 @@ public class RightClickAndMilk
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
